e888d73e43175d264c838c455c2380c12109ebee
zamir <zamir357@icloud.com>
2024-09-02 21:03:30 -0400

    Assigment 1 9/2/2024

---
+++ assigment_1.py
@@ -0,0 +1,19 @@
+input('start calculations\n')
+answer1= 5*6
+print('the answer to your query is',answer1,'\n')
+input('next\n')
+answer2 = 8/3
+print('the answer to your query',answer2,'\n')
+input('next\n')
+answer3 = int(8/3)
+print('the asnwer to your query is',answer3,'\n')
+input('next\n')
+answer4 = 8%3
+print('the answer to your query is',answer4,'\n')
+input('next\n')
+answer5 = 4200000+23090
+print('the asswer to your query is',answer5,'\n')
+input('next\n')
+answer6 = 4200000-7633
+print('the answer to your query is',answer6,'\n')
+print('Zamir Colina')
